fd8a17e9ffb8e8fecd4c1c08bf48dac0305ed377
James Zheng <jzzheng@gmail.com>
2017-09-22 09:28:03 +0800

    Update secret_lottery_winning_no3.py

--- secret_lottery_winning_no3.py
+++ secret_lottery_winning_no3.py
@@ -1,9 +1,9 @@
 ### QUESTION:
 '''
     Mr. X is approached in the subway by a guy who claims to be an alien stranded on Earth and to possess time machine
-that allows him to know the future. He needs funds to fix his flying saucer but filling in wiMAX_NOing numbers for next week's lottery
-would create a time paradox. Therefore, he's willing to sell next week's wiMAX_NOing numbers to Mr. X at a favorable price. Mr. X,
-as gullible as he is, feels that this may be a scam and asks for a proof. Alien gives him the next week's wiMAX_NOing numbers in
+that allows him to know the future. He needs funds to fix his flying saucer but filling in winning numbers for next week's lottery
+would create a time paradox. Therefore, he's willing to sell next week's winning numbers to Mr. X at a favorable price. Mr. X,
+as gullible as he is, feels that this may be a scam and asks for a proof. Alien gives him the next week's winning numbers in
 encrypted form so that Mr. X can't use them and then decide not to pay for them. After the lottery draw he'll give Mr. X the key
 to unlock the file and Mr. X can verify that the prediction was correct.
 After the draw, Mr. X gets the key and lo and behold, the numbers are correct! To rule out the possibility that it happened by chance,
@@ -26,30 +26,30 @@ encode = lambda d: ('0'*NCODE+bin(keyd[d])[2:])[-NCODE:]
 decode = lambda p: eval('0b'+''.join([('0'*UNIT+bin(asciidr[i])[2:])[-UNIT:] for i in p]))
 
 # main logic
-gen_encrypted_wiMAX_NOing_no = lambda : ''.join(random.sample(string.ascii_letters*64,64))
+gen_encrypted_winning_no = lambda : ''.join(random.sample(string.ascii_letters*64,64))
 gen_decrypt_key = lambda _, data : ''.join([asciid[eval('0b'+encode(data)[i*UNIT:(i+1)*UNIT])] for i in range(NDIGIT)])
-decrypt_wiMAX_NOing_no = lambda _, passwd: keydr[decode(passwd)]
+decrypt_winning_no = lambda _, passwd: keydr[decode(passwd)]
 
 ### USE CASE:
 # give fake encrypted no. to mr. X
-locked_wiMAX_NOing_no_to_MrX = gen_encrypted_wiMAX_NOing_no()
-print 'locked_wiMAX_NOing_no_to_MrX :\n\t', locked_wiMAX_NOing_no_to_MrX
+locked_winning_no_to_MrX = gen_encrypted_winning_no()
+print 'locked_winning_no_to_MrX :\n\t', locked_winning_no_to_MrX
 
-# wait for actual wiMAX_NOing no. is out and generate fake key
+# wait for actual winning no. is out and generate fake key
 WINNING_NO = (3,11,12,27,31)
-key_to_unlock = gen_decrypt_key(locked_wiMAX_NOing_no_to_MrX, WINNING_NO)
+key_to_unlock = gen_decrypt_key(locked_winning_no_to_MrX, WINNING_NO)
 print 'key_to_unlock :\n\t', key_to_unlock
 
 # mr. X uses the fake key to decrypt the fake encrpted no.
-unlocked_wiMAX_NOing_no_to_MrX = decrypt_wiMAX_NOing_no(locked_wiMAX_NOing_no_to_MrX, key_to_unlock)
-print 'unlocked_wiMAX_NOing_no_to_MrX :\n\t', unlocked_wiMAX_NOing_no_to_MrX
+unlocked_winning_no_to_MrX = decrypt_winning_no(locked_winning_no_to_MrX, key_to_unlock)
+print 'unlocked_winning_no_to_MrX :\n\t', unlocked_winning_no_to_MrX
 
 ### OUTPUT:
 '''
-locked_wiMAX_NOing_no_to_MrX :
+locked_winning_no_to_MrX :
 	KCpmAZDzCtXExsVIcAkvMsItwbUMkcUxeQQkjYZtVHPGjzeDlEsveioNVtLeSfHJ
 key_to_unlock :
 	sBC
-unlocked_wiMAX_NOing_no_to_MrX :
+unlocked_winning_no_to_MrX :
 	(3, 11, 12, 27, 31)
 '''
